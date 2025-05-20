ab3cb299f19a43ed71d2a85bc1c04e9a9452aa16
ye-yint-kyaw <yeyintkyaw.sgg@gmail.com>
2025-05-20 09:50:34 +0630

    Update Nav bar

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { Search } from "lucide-react"
 import IMG_LINK from "../assets/NUCS_Logo_White.png"
 export const Layout = ({children}) => {
     return (
@@ -6,12 +7,24 @@ export const Layout = ({children}) => {
         {/* Insert navigation link layout and styles here */}
             <nav className=" bg-[#008282] shadow-md">
 
-                <div className="flex justify-between items-center py-8">
-                    <div className="w-[50%]">
+                <div className="flex justify-between items-center py-8 px-2 sm:px-15">
+                    <div className="">
                         <Link  to="/"> <img className="w-[200px]" src={IMG_LINK} alt="NUCS LOGO" /> </Link>
                     </div>
-                    <div className="w-[50%]">
-                        Search Area/ Login/Out and Profile Area
+                    <div className="flex">
+                        <div className="relative w-full mx-3 max-w-md">
+                            <input
+                            type="text"
+                            placeholder="Search something..."
+                            className="hidden sm:block w-full pl-12 pr-4 py-3 rounded-2xl shadow-lg bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300"
+                            />
+                            <Link><Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400" /></Link>
+                        </div>
+                        <button
+                        className="border-2 border-white text-white px-6 py-2 cursor-pointer rounded-2xl hover:bg-[#fff] hover:text-[#008282] transition duration-300"
+                        >
+                            Login
+                        </button>
                     </div>
                 </div>
                 <div className="hidden w-full bg-[#005252] sm:flex box-border">
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,11 @@ import { Layout } from "../Layout/Layout"
 export const Home = () => {
     return (
         <Layout>
-            This is from HOME.
+            <div className="">
+                <div className="mx-2 sm:mx-15">
+                    <h1 className="h-[500px]">This is HOME</h1>
+                </div>
+            </div>
         </Layout>
     )
 }
